2ca2e8a137cd78aca0351b08d4e5e13ec9010c61
Luis Daniel Martinez <luisdanielcurso@hotmail.com>
2019-07-09 00:51:39 -0400

    Initial commit

---
+++ app/src/main/java/com/example/obtenertextimagenocr/TextDisplay.java
@@ -0,0 +1,23 @@
+package com.example.obtenertextimagenocr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TextDisplay extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_display);
+
+        textView = (TextView) findViewById(R.id.textview_result);
+
+        Bundle bundle = getIntent().getExtras();
+        String result = bundle.getString("DATA");
+
+        textView.setText(result);
+
+    }
+}
